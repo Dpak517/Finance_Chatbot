3716ac4568ed1e443aa66a9c3819990e9bc4ae74
Dpak517 <deepakbedwal20@gmail.com>
2026-06-10 23:31:49 +0530

    Refactor Finance Chatbot with Groq API integration
    Updated the Finance Chatbot to include Groq API key input and adjusted processing logic for URLs and PDF uploads.

--- main.py
+++ main.py
@@ -2,24 +2,46 @@ import streamlit as st
 import os
 import pickle
 from langchain_community.document_loaders import PyPDFLoader
-import tempfile
+from torchvision.transforms.v2 import functional as tvF
 from langchain_groq import ChatGroq
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint, HuggingFaceEmbeddings
 from bs4 import BeautifulSoup
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 from langchain_community.vectorstores import FAISS
 from langchain_classic.chains import create_retrieval_chain
 from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-load_dotenv()
+#load_dotenv()
 st.header('**Finance Chatbot** 📈')
 #st.header(':rainbow[**Finance Chatbot**]')
+
+
+st.sidebar.title('Enter Groq API key')
+groq_api_key=st.sidebar.text_input(
+    'Enter Groq API key here',
+    type="password",
+    #placeholder="gsk_.."
+)
+
+if groq_api_key!='':
+    if not groq_api_key.startswith('gsk_'):
+        st.sidebar.warning("Enter valid Groq API key")
+        if not groq_api_key:
+            st.sidebar.warning("First enter Groq API key")
+
+
+# if not groq_api_key:
+#     st.sidebar.warning("First enter Groq API key")
+
+
+
+
 st.sidebar.title('NEWS Article links')
-embeddings=HuggingFaceEmbeddings()
+embeddings=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 processor=st.empty()
 urls=[]
 for i in range(3):
@@ -27,17 +49,18 @@ for i in range(3):
     if url.startswith('http://') or url.startswith('https://'):
         urls.append(url)
 
-processed_url=st.sidebar.button('Process URLs')
-if processed_url:
-    loader=WebBaseLoader(urls)
-    url_data=loader.load()
-    processor.text('data loading ....')
-    splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=20)
-    docs = splitter.split_documents(url_data)
-    embeddings = embeddings
-    processor.text('embedding urls ....')
-    vector_store = FAISS.from_documents(docs, embeddings)
-    vector_store.save_local('faiss_index')
+# processed_url=st.sidebar.button(' Start Processing ')
+# if processed_url:
+#     loader=WebBaseLoader(urls)
+#     url_data=loader.load()
+#     processor.text('data loading ....')
+#     splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=20)
+#     docs = splitter.split_documents(url_data)
+#     embeddings = embeddings
+#     processor.text('embedding urls ....')
+#     vector_store = FAISS.from_documents(docs, embeddings)
+#     vector_store.save_local('faiss_index')
+#     processor.text('')
 
 st.sidebar.markdown('Upload PDF')
 uploaded_file=st.sidebar.file_uploader(label='Upload PDF',type='pdf')
@@ -55,14 +78,43 @@ if uploaded_file:
     splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=20)
     docs = splitter.split_documents(pdf_data)
     embeddings = embeddings
+    processor.text('embedding text ....')
+    vector_store = FAISS.from_documents(docs, embeddings)
+    vector_store.save_local('faiss_index')
+    processor.text('')
+
+processed_url=st.sidebar.button(' Start Processing ')
+if processed_url:
+    loader=WebBaseLoader(urls)
+    url_data=loader.load()
+    processor.text('data loading ....')
+    splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=20)
+    docs = splitter.split_documents(url_data)
+    embeddings = embeddings
     processor.text('embedding urls ....')
     vector_store = FAISS.from_documents(docs, embeddings)
     vector_store.save_local('faiss_index')
+    processor.text('')
+
+
+
+
+
+
+
+
+
+if not groq_api_key:
+    st.sidebar.warning("First enter Groq API key")
 
 query = st.text_input('Question: ')
-model = ChatGroq(
-    model="llama-3.1-8b-instant",
-    temperature=1.9)
+if groq_api_key:
+    model=ChatGroq(
+        api_key=groq_api_key,
+        model="llama-3.1-8b-instant",
+        temperature=1.9)
+    
+
 prompt = PromptTemplate(
     template="""
 Answer the question using only the provided context and elaborate the answer like proper sentence.
@@ -107,5 +159,7 @@ if query:
                 st.write(f"🔗 {src}")
         else:
             st.write("No sources found")
+    #st.write(result['answer'])
+
     #st.write(result['answer'])
 
